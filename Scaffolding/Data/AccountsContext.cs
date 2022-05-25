f3e6ad20af37f4de5c30029e610927a647afef62
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 12:04:29 +0530

    first commit

---
+++ Scaffolding/Data/AccountsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Scaffolding.Models;
+
+namespace Scaffolding.Data
+{
+    public class AccountsContext : DbContext
+    {
+        public AccountsContext (DbContextOptions<AccountsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Scaffolding.Models.Account> Account { get; set; }
+    }
+}
---
+++ Scaffolding/Migrations/20220204050236_CreatedAccountsDatabase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Scaffolding.Migrations
+{
+    public partial class CreatedAccountsDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    AccountId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.AccountId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Account");
+        }
+    }
+}
